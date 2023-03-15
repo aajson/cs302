85d1c0d4e7ceaa0521e4f95e6db601a2c29cba42
aajson <aajson11@gmail.com>
2023-03-15 15:20:35 -0600

    help
    pleas

--- poject on1/sketch.js
+++ poject on1/sketch.js
@@ -8,14 +8,35 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noFill()
 }
 
 function draw() {
   background(220);
+  gears(mouseX, mouseY,100);
 }
 
 
-function gears(x,y,r,f){
-  
+function gears(x,y,r,n){
+  // iner gear
+  ellipse(x,y,r);
+  // outer gear
+  outerGears(x,y,n);
+
+}
+
+
+function outerGears(x,y){
+  for (let i = -90; i <= 360; i +=90){
+    if (i){
+      arc(x,y,200,200,i,i+90,); 
+    
+ 
+    }
+    else{
+      arc(x,y,150,150,i,i+90,);
+    }
+  }
+
 
 }
