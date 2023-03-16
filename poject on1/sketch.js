df6c6e6767ba086a28b137f889a9db3126a4a412
aajson <aajson11@gmail.com>
2023-03-16 15:16:23 -0600

    yep yep day 14
    cog is good

--- poject on1/sketch.js
+++ poject on1/sketch.js
@@ -5,15 +5,18 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let r=15;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noFill()
+  noFill();
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(220);
-  gears(mouseX, mouseY,100);
+  sizescale(mouseWheel());
+  gears(mouseX, mouseY,r,45);
 }
 
 
@@ -21,22 +24,35 @@ function gears(x,y,r,n){
   // iner gear
   ellipse(x,y,r);
   // outer gear
-  outerGears(x,y,n);
+  outerGears(x,y,r,n);
 
 }
 
 
-function outerGears(x,y){
-  for (let i = -90; i <= 360; i +=90){
-    if (i){
-      arc(x,y,200,200,i,i+90,); 
+function outerGears(x,y,r,n){
+  for (let i = -n; i <= 360; i +=n){
     
- 
+      
+    
+    //outer cog
+    if (i<=360){
+      arc(x,y,r*2,r*2,i,i+n/2); 
+    
     }
-    else{
-      arc(x,y,150,150,i,i+90,);
+    //iner cog
+    if (i<=360){ 
+      arc(x,y,r*1.5,r*1.5,i-n/2,i);
     }
   }
+  
 
+}
+function sizescale(thing){
+  if (thing.DeltaY > 0){
+    r += 50;
+  }
+  else if (thing.DeltaY < 0){
+    r -= 50;
 
+  }
 }
