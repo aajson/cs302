07d8beb8ff96181f6baab065a31cfc22d32953eb
aajson <aajson11@gmail.com>
2023-03-17 15:28:22 -0600

    add spin stuf
    gey it done

--- poject on1/sketch.js
+++ poject on1/sketch.js
@@ -6,26 +6,46 @@
 // - describe what you did to take this project "above and beyond"
 
 let r=15;
-
+let allgear=[];
+let n=45;
+let s =15;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noFill();
   angleMode(DEGREES);
 }
+  
 
 function draw() {
   background(220);
-  sizescale(mouseWheel());
-  gears(mouseX, mouseY,r,45);
+  for (const gear of allgear) {
+    gears(gear.x,gear.y,gear.r,gear.n,gear.s);
+    
+    
+
+    
+
+  }
+  gears(mouseX, mouseY,r,n,s);
+  if (keyIsDown(LEFT_ARROW)) {
+    s -= 50; 
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    s += 50;
+  }
 }
 
 
-function gears(x,y,r,n){
+function gears(x,y,r,n,spin){
   // iner gear
-  ellipse(x,y,r);
+  push();
+  translate(x,y);
+  rotate(spin);
+  
+  ellipse(0,0,r);
   // outer gear
-  outerGears(x,y,r,n);
-
+  outerGears(0,0,r,n);
+  pop();
 }
 
 
@@ -47,12 +67,28 @@ function outerGears(x,y,r,n){
   
 
 }
-function sizescale(thing){
-  if (thing.DeltaY > 0){
+function mouseWheel(event){
+  if (event.delta > 0){
     r += 50;
   }
-  else if (thing.DeltaY < 0){
+  else if (event.delta < 0){
     r -= 50;
 
   }
 }
+
+
+
+
+function mousePressed(){
+  let gear={
+    x:mouseX,
+    y:mouseY,
+    r:r,
+    n:n,
+    s:s,
+
+  };
+  allgear.push(gear);
+}
+
